881258f0881abe469cfcdb6bac1e33a4622136a7
Alxandr <alxandr@alxandr.me>
2015-09-14 05:51:19 +0200

    Attempting deployment

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e # exit with nonzero exit code if anything fails
+
+# This script runs after `gulp dist`. The deployment files are in dist/sample.
+
+if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+  pushd dist/sample
+  git init
+
+  # inside this git repo we'll pretend to be a new user
+  git config user.name "Travis CI"
+  git config user.email "alxandr@alxandr.me"
+
+  # The first and only commit to this new Git repo contains all the
+  # files present with the commit message "Deploy to GitHub Pages".
+  git add .
+  git commit -m "Deploy to GitHub Pages"
+
+  # Force push from the current repo's master branch to the remote
+  # repo's gh-pages branch. (All previous history on the gh-pages branch
+  # will be lost, since we are overwriting it.) We redirect any output to
+  # /dev/null to hide any sensitive credential data that might otherwise be exposed.
+  git push --force --quiet "https://${GH_TOKEN}@github.com/${GH_REPO}" master:gh-pages > /dev/null 2>&1
+  popd
+else
+  if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+    echo "Skipping deployment because it's a pull request"
+  else
+    echo "Skipping deployment because it's not master branch"
+  fi
+fi
+
